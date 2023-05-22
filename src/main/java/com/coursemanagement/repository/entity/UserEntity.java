f56e9f6b4a9b4394089afbb7d7ec97b4ff33210c
Yaroslav Yarmoshyk <yarmoshyk96@gmail.com>
2023-05-22 19:16:06 +0300

    Fix LIE

--- src/main/java/com/coursemanagement/repository/entity/UserEntity.java
+++ src/main/java/com/coursemanagement/repository/entity/UserEntity.java
@@ -44,7 +44,7 @@ public class UserEntity {
     @Enumerated(value = EnumType.STRING)
     @Column(name = "status")
     private UserStatus status;
-    @ManyToMany(fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "user_role",
             joinColumns = @JoinColumn(name = "user_id"),

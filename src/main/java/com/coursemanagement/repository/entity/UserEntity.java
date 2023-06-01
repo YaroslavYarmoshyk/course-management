d484a30c96e767ab7058e749a4c9f1222bf3be21
Yaroslav Yarmoshyk <yarmoshyk96@gmail.com>
2023-06-01 20:28:23 +0300

    Remove bidirectional relation from user

--- src/main/java/com/coursemanagement/repository/entity/UserEntity.java
+++ src/main/java/com/coursemanagement/repository/entity/UserEntity.java
@@ -1,7 +1,6 @@
 package com.coursemanagement.repository.entity;
 
 import com.coursemanagement.enumeration.UserStatus;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -12,7 +11,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -69,13 +67,6 @@ public class UserEntity {
             inverseJoinColumns = @JoinColumn(name = "role_id")
     )
     private Set<RoleEntity> roles = new HashSet<>();
-
-    @OneToMany(
-            mappedBy = "userEntity",
-            cascade = CascadeType.ALL,
-            orphanRemoval = true
-    )
-    private Set<UserCourseEntity> courses = new HashSet<>();
 
     @Override
     public boolean equals(final Object o) {

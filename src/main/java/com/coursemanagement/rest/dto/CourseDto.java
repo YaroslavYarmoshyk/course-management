574ce3221f04c84f51a98321b562a537d6f9eb9f
Yaroslav <yarmoshyk96@gmail.com>
2023-06-11 22:30:02 +0300

    Add few constructors of CourseDto record

--- src/main/java/com/coursemanagement/rest/dto/CourseDto.java
+++ src/main/java/com/coursemanagement/rest/dto/CourseDto.java
@@ -1,6 +1,7 @@
 package com.coursemanagement.rest.dto;
 
 import com.coursemanagement.enumeration.UserCourseStatus;
+import com.coursemanagement.model.UserCourse;
 import com.coursemanagement.repository.entity.CourseEntity;
 
 public record CourseDto(
@@ -10,6 +11,20 @@ public record CourseDto(
         UserCourseStatus status
 ) {
     public CourseDto(final CourseEntity entity, final UserCourseStatus status) {
-        this(entity.getCode(), entity.getTitle(), entity.getDescription(), status);
+        this(
+                entity.getCode(),
+                entity.getTitle(),
+                entity.getDescription(),
+                status
+        );
+    }
+
+    public CourseDto(final UserCourse userCourse) {
+        this(
+                userCourse.getCourse().getCode(),
+                userCourse.getCourse().getTitle(),
+                userCourse.getCourse().getDescription(),
+                userCourse.getStatus()
+        );
     }
 }

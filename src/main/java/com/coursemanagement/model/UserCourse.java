a8e02c36973c016dfe4f65bf9b50472d5afa589f
Yaroslav <yarmoshyk96@gmail.com>
2023-06-11 22:29:03 +0300

    Refactor user course model

--- src/main/java/com/coursemanagement/model/UserCourse.java
+++ src/main/java/com/coursemanagement/model/UserCourse.java
@@ -3,12 +3,14 @@ package com.coursemanagement.model;
 import com.coursemanagement.enumeration.UserCourseStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserCourse {
     private Long id;
     private User user;
     private Course course;
-    private UserCourseStatus userCourseStatus;
+    private UserCourseStatus status;
 }

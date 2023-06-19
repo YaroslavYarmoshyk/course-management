e19346aed5a84d8d5c4f13d7817f6a719bd85058
Yaroslav Yarmoshyk <yarmoshyk96@gmail.com>
2023-06-19 20:46:43 +0300

    Add annotation which allows getting current user. Also add AuthorizationUtil class

---
+++ src/main/java/com/coursemanagement/util/AuthorizationUtil.java
@@ -0,0 +1,32 @@
+package com.coursemanagement.util;
+
+import com.coursemanagement.enumeration.Role;
+import com.coursemanagement.model.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Collection;
+import java.util.Objects;
+import java.util.Optional;
+
+public final class AuthorizationUtil {
+
+    public static boolean isAdmin() {
+        return Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
+                .map(Authentication::getAuthorities)
+                .stream()
+                .flatMap(Collection::stream)
+                .map(GrantedAuthority::getAuthority)
+                .map(Role::of)
+                .anyMatch(role -> Objects.equals(role, Role.ADMIN));
+    }
+
+    public static boolean isAdmin(final User user) {
+        return Optional.ofNullable(user)
+                .map(User::getRoles)
+                .stream()
+                .flatMap(Collection::stream)
+                .anyMatch(role -> Objects.equals(role, Role.ADMIN));
+    }
+}

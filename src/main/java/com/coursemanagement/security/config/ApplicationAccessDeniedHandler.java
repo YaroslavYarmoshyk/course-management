bb159dcda282aac84feb43adad6a0790fbf24ef5
Yaroslav Yarmoshyk <yarmoshyk96@gmail.com>
2023-06-23 20:48:38 +0300

    Remove magic number

--- src/main/java/com/coursemanagement/security/config/ApplicationAccessDeniedHandler.java
+++ src/main/java/com/coursemanagement/security/config/ApplicationAccessDeniedHandler.java
@@ -3,6 +3,7 @@ package com.coursemanagement.security.config;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
@@ -18,6 +19,6 @@ public class ApplicationAccessDeniedHandler implements AccessDeniedHandler {
                        final HttpServletResponse response,
                        final AccessDeniedException accessDeniedException) throws IOException {
         log.warn(accessDeniedException.getClass().getSimpleName(), accessDeniedException);
-        response.sendError(403, "Access Denied");
+        response.sendError(HttpStatus.FORBIDDEN.value(), "Access Denied");
     }
 }

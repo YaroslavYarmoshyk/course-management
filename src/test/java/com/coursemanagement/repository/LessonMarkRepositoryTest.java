0ac2e6a462752ad81f7b3409f7c4b7b531484074
Yaroslav <yarmoshyk96@gmail.com>
2023-07-12 20:40:55 +0300

    add lesson mark repository tests

---
+++ src/test/java/com/coursemanagement/repository/LessonMarkRepositoryTest.java
@@ -0,0 +1,85 @@
+package com.coursemanagement.repository;
+
+import com.coursemanagement.config.DatabaseSetupExtension;
+import com.coursemanagement.config.properties.UserTestDataProperties;
+import com.coursemanagement.model.User;
+import com.coursemanagement.repository.entity.LessonEntity;
+import com.coursemanagement.repository.entity.LessonMarkEntity;
+import com.coursemanagement.repository.entity.UserEntity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@DataJpaTest(showSql = false)
+@ExtendWith(DatabaseSetupExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@EnableConfigurationProperties(UserTestDataProperties.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class LessonMarkRepositoryTest {
+    @Autowired
+    private LessonMarkRepository lessonMarkRepository;
+    @Autowired
+    private UserTestDataProperties userTestDataProperties;
+    private User student;
+    private User instructor;
+
+    @BeforeEach
+    void setUp() {
+        student = userTestDataProperties.getStudent();
+        instructor = userTestDataProperties.getInstructor();
+    }
+
+    @Test
+    @Order(1)
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
+    @DisplayName(value = "Test getting lesson mark by id with student")
+    @Sql(value = "/scripts/add_lesson_marks.sql")
+    void testFindLessonMarkById_Student_Is_Fetched() {
+        final LessonMarkEntity lessonMarkEntity = lessonMarkRepository.findLessonMarkById(1L);
+        assertNotNull(lessonMarkEntity);
+        final UserEntity studentEntity = lessonMarkEntity.getStudent();
+        assertNotNull(studentEntity);
+        assertEquals(studentEntity.getId(), student.getId());
+    }
+
+    @Test
+    @Order(2)
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
+    @DisplayName(value = "Test getting lesson mark by id with lesson")
+    @Sql(value = "/scripts/add_lesson_marks.sql")
+    void testFindLessonMarkById_Lesson_Is_Fetched() {
+        final LessonMarkEntity lessonMarkEntity = lessonMarkRepository.findLessonMarkById(1L);
+        assertNotNull(lessonMarkEntity);
+        final LessonEntity lessonEntity = lessonMarkEntity.getLesson();
+        assertNotNull(lessonEntity);
+        assertEquals(lessonEntity.getTitle(), "Introduction to Algebra");
+    }
+
+    @Test
+    @Order(3)
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
+    @DisplayName(value = "Test getting lesson mark by id with instructor")
+    @Sql(value = "/scripts/add_lesson_marks.sql")
+    void testFindLessonMarkById_Instructor_Is_Fetched() {
+        final LessonMarkEntity lessonMarkEntity = lessonMarkRepository.findLessonMarkById(1L);
+        assertNotNull(lessonMarkEntity);
+        final UserEntity instructorEntity = lessonMarkEntity.getInstructor();
+        assertNotNull(instructorEntity);
+        assertEquals(instructorEntity.getId(), instructor.getId());
+        assertEquals(instructorEntity.getEmail(), instructor.getEmail());
+    }
+}

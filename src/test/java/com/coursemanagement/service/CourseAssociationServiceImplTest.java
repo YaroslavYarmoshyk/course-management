7ddf464f6f6b704ca748f1d5d1347682254b8614
Yaroslav Yarmoshyk <yarmoshyk96@gmail.com>
2023-08-31 13:40:00 +0300

    Move course association service test to common package

--- src/test/java/com/coursemanagement/service/CourseAssociationServiceImplTest.java
+++ src/test/java/com/coursemanagement/service/CourseAssociationServiceImplTest.java
@@ -1,6 +1,7 @@
-package com.coursemanagement.service.impl;
+package com.coursemanagement.service;
 
 import com.coursemanagement.repository.CourseRepository;
+import com.coursemanagement.service.impl.CourseAssociationServiceImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;

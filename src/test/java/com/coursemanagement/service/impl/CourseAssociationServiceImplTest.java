a3c1c5ae7460c52e87158500fa5f3f58b7bd415b
Yaroslav Yarmoshyk <yarmoshyk96@gmail.com>
2023-08-31 13:38:31 +0300

    Add course association service test

---
+++ src/test/java/com/coursemanagement/service/impl/CourseAssociationServiceImplTest.java
@@ -0,0 +1,36 @@
+package com.coursemanagement.service.impl;
+
+import com.coursemanagement.repository.CourseRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static com.coursemanagement.util.TestDataUtils.FIRST_STUDENT;
+import static com.coursemanagement.util.TestDataUtils.getRandomUserCourseByUser;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(value = MockitoExtension.class)
+class CourseAssociationServiceImplTest {
+    @InjectMocks
+    @Spy
+    private CourseAssociationServiceImpl courseAssociationService;
+    @Mock
+    private CourseRepository courseRepository;
+
+    @Test
+    @DisplayName("Test user course association")
+    void testUserCourseAssociation() {
+        final Long studentId = FIRST_STUDENT.getId();
+        final Long courseCode = getRandomUserCourseByUser(FIRST_STUDENT).getCourse().getCode();
+        when(courseRepository.existsByUserCoursesUserIdAndCode(studentId, courseCode)).thenReturn(true);
+
+        courseAssociationService.isUserAssociatedWithCourse(studentId, courseCode);
+
+        verify(courseRepository).existsByUserCoursesUserIdAndCode(studentId, courseCode);
+    }
+}

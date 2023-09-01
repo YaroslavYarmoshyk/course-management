01fc6229c32b26c2041c0399a31c5e0c207eaa33
Yaroslav Yarmoshyk <yarmoshyk96@gmail.com>
2023-09-01 15:29:30 +0300

    Add user association service tests

--- src/test/java/com/coursemanagement/service/UserAssociationServiceImplTest.java
+++ src/test/java/com/coursemanagement/service/UserAssociationServiceImplTest.java
@@ -1,11 +1,16 @@
 package com.coursemanagement.service;
 
+import com.coursemanagement.model.LessonContent;
 import com.coursemanagement.repository.CourseRepository;
 import com.coursemanagement.repository.LessonContentRepository;
 import com.coursemanagement.repository.LessonRepository;
 import com.coursemanagement.service.impl.UserAssociationServiceImpl;
+import org.instancio.Instancio;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -17,6 +22,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(value = MockitoExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class UserAssociationServiceImplTest {
     @InjectMocks
     private UserAssociationServiceImpl userAssociationService;
@@ -27,6 +33,7 @@ class UserAssociationServiceImplTest {
     @Mock
     private LessonContentRepository lessonContentRepository;
 
+    @Order(1)
     @Test
     @DisplayName("Test user course association")
     void testUserCourseAssociation() {
@@ -37,5 +44,34 @@ class UserAssociationServiceImplTest {
         userAssociationService.isUserAssociatedWithCourse(studentId, courseCode);
 
         verify(courseRepository).existsByUserCoursesUserIdAndCode(studentId, courseCode);
+    }
+
+    @Order(2)
+    @Test
+    @DisplayName("Test user lesson association")
+    void testUserLessonAssociation() {
+        final Long studentId = FIRST_STUDENT.getId();
+        final Long lessonId = 1L;
+        when(lessonRepository.existsByCourseUserCoursesUserIdAndId(studentId, lessonId)).thenReturn(true);
+
+        userAssociationService.isUserAssociatedWithLesson(studentId, lessonId);
+
+        verify(lessonRepository).existsByCourseUserCoursesUserIdAndId(studentId, lessonId);
+    }
+
+    @Order(3)
+    @Test
+    @DisplayName("Test user lesson file association")
+    void testUserLessonFileAssociation() {
+        final LessonContent lessonContent = Instancio.create(LessonContent.class);
+        final Long studentId = FIRST_STUDENT.getId();
+        final Long fileId = lessonContent.getFileId();
+        final Long lessonId = lessonContent.getLessonId();
+        when(lessonContentRepository.findByFileId(fileId)).thenReturn(lessonContent);
+        when(lessonRepository.existsByCourseUserCoursesUserIdAndId(studentId, lessonId)).thenReturn(true);
+
+        userAssociationService.isUserAssociatedWithLessonFile(studentId, fileId);
+
+        verify(lessonRepository).existsByCourseUserCoursesUserIdAndId(studentId, lessonId);
     }
 }

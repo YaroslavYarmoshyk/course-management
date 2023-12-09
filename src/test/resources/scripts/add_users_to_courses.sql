14eb5d2ee4c361d77cdd61b4e313d69dbd8e3a53
Yaroslav <yarmoshyk96@gmail.com>
2023-12-09 13:21:21 +0200

    Add course information e2e tests

--- src/test/resources/scripts/add_users_to_courses.sql
+++ src/test/resources/scripts/add_users_to_courses.sql
@@ -1,7 +1,8 @@
 INSERT INTO "user" (id, first_name, last_name, email, "password", phone, status)
 VALUES (1, 'John', 'Smith', 'john-smith@gmail.com', '$2a$10$2DXHDU2uYMrlIiRHVb2CletM6kMjhLmvHzgyfOHQlV2SrLzE17hSK', '+380974309331', 'A'),
        (2, 'Marry', 'Poppins', 'poppins@yahoo.com', '$2a$10$Zd/kQRuvnJ2br8dN5OyWt.LskBDAHL1UpZoRdbdOz7QcmNPJYJdYe', '+380971668744', 'A'),
-       (3, 'Tyrion', 'Lannister', 'goldlannister@gmail.com', '$2a$10$hbhWFqEqpikKav8uAGP7WOzTUxAz..TJkVnloLn0bS6vbw4aGqD/m', '+380971205151', 'A')
+       (3, 'Tyrion', 'Lannister', 'goldlannister@gmail.com', '$2a$10$hbhWFqEqpikKav8uAGP7WOzTUxAz..TJkVnloLn0bS6vbw4aGqD/m', '+380971205151', 'A'),
+       (5, 'Thor', 'Odinson', 'thor-odinson96@gmail.com', '$2a$12$27wtXbRuvH0m5hzwY5QJwOKAFuhbPziXUDW9Ijse5n0.7XaFvbH2q', '+380985225152', 'A')
 ON CONFLICT (email) DO NOTHING;
 
 INSERT INTO user_role (user_id, role_id)
@@ -23,5 +24,7 @@ INSERT INTO user_course (user_id, course_code, status, enrollment_date)
 VALUES (2, 22324, 'S', NOW()),
        (3, 34432, 'S', NOW()),
        (3, 56548, 'S', NOW()),
-       (2, 76552, 'S', NOW())
+       (2, 76552, 'S', NOW()),
+       (2, 56548, 'S', NOW()),
+       (5, 56548, 'S', NOW())
 ON CONFLICT (user_id, course_code) DO NOTHING;

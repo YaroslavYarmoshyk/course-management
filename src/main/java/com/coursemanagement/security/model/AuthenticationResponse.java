09cc9902e731a08d7d18886594b29584972e7f75
Yaroslav <yarmoshyk96@gmail.com>
2024-01-27 21:42:44 +0200

    Temporary disable allure 221

--- src/main/java/com/coursemanagement/security/model/AuthenticationResponse.java
+++ src/main/java/com/coursemanagement/security/model/AuthenticationResponse.java
@@ -1,3 +1,6 @@
 package com.coursemanagement.security.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record AuthenticationResponse(String token) {}
